1280dd7a003d852e0b723ff7320d3d66155b773d
Meet Kasediya <meetkasediya2004@gmail.com>
2026-03-03 16:03:08 +0530

    fixes

--- public/game.js
+++ public/game.js
@@ -1,11 +1,15 @@
 
 const socket = io();
+let username = prompt("What should they call you?:");
+if (!username) username = "Player";
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 function resizeCanvas() {
-  canvas.width = canvas.clientWidth;
-  canvas.height = canvas.clientHeight;
+  const rect = canvas.getBoundingClientRect();
+  canvas.width = rect.width;
+  canvas.height = rect.height;
 }
 
 resizeCanvas();
@@ -15,7 +19,12 @@ window.addEventListener("resize", resizeCanvas);
 const logBox = document.getElementById("logBox");
 console.log("LogBox:", logBox);
 
-let player = { x: 100, y: 100 };
+let player = { 
+  x: 100, 
+  y: 100,
+  name: username
+};
+
 let otherPlayer = { x: 200, y: 200 };
 
 document.addEventListener("keydown", (e) => {
@@ -24,6 +33,10 @@ document.addEventListener("keydown", (e) => {
   if (e.key === "ArrowLeft") player.x -= 10;
   if (e.key === "ArrowRight") player.x += 10;
 
+
+
+  player.x = Math.max(0, Math.min(player.x, canvas.width - 30));
+  player.y = Math.max(0, Math.min(player.y, canvas.height - 30));
   socket.emit("move", player);
 });
 
@@ -41,13 +54,23 @@ p.textContent = `[${new Date().toLocaleTimeString()}] ${message}`;
 });
 
 function draw() {
-  ctx.clearRect(0, 0, 500, 500);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // BLUE PLAYER
   ctx.fillStyle = "blue";
   ctx.fillRect(player.x, player.y, 30, 30);
 
+  ctx.fillStyle = "white";
+  ctx.font = "12px monospace";
+  ctx.textAlign = "center";
+  ctx.fillText(player.name, player.x + 15, player.y + 45);
+
+  // RED PLAYER
   ctx.fillStyle = "red";
   ctx.fillRect(otherPlayer.x, otherPlayer.y, 30, 30);
+
+  ctx.fillStyle = "white";
+  ctx.fillText(otherPlayer.name, otherPlayer.x + 15, otherPlayer.y + 45);
 
   requestAnimationFrame(draw);
 }
